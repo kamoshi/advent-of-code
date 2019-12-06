c0588e6e0e4e1ca502d17f99b322f377e79ecb6b
kamoshi <18511281+kamoshi@users.noreply.github.com>
2019-12-06 23:11:11 +0100

    Delete Jump.scala

--- 2019/day5/opcode/Jump.scala
+++
@@ -1,8 +0,0 @@
-package day5.opcode
-
-trait Jump extends OpCode
-{
-  /** Executes instruction for given parameters and modes */
-  def checkConditionAndJump(tape: Array[Int], param1: Int, param2: Int, param3: Int, mode1: Int, mode2: Int, mode3: Int): (Boolean, Int)
-}
-
