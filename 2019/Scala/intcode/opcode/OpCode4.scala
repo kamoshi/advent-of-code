9723194cb94d8c85fd05a01e79afa7637f4dd6b0
kamoshi <18511281+kamoshi@users.noreply.github.com>
2019-12-09 22:24:54 +0100

    Update OpCode4.scala

--- 2019/Scala/intcode/opcode/OpCode4.scala
+++ 2019/Scala/intcode/opcode/OpCode4.scala
@@ -1,13 +1,15 @@
 package intcode.opcode
 
-/** Output */
-case object OpCode4 extends Output
+/** Output
+ *  Outputs the value of its only parameter.
+ */
+case object OpCode4 extends OpCode
 {
   /** length of an instruction */
   override val length: Int = 2
 
   /** Executes instruction for given parameters and modes */
-  override def output(tape: Array[Long], relative: Long, param1: Long, mode1: Int): Long =
+  def output(tape: Array[Long], relative: Long, param1: Long, mode1: Int): Long =
   {
     accessor(relative, param1, mode1, tape)
   }
