0ef4113ccb195971d96603314fda068d7f4aa74a
kamoshi <18511281+kamoshi@users.noreply.github.com>
2019-12-09 22:25:50 +0100

    Delete Input.scala

--- 2019/Scala/intcode/opcode/Input.scala
+++
@@ -1,7 +0,0 @@
-package intcode.opcode
-
-trait Input extends OpCode
-{
-  /** Executes instruction for given parameters and modes */
-  def input(tape: Array[Long], relative: Long, param1: Long, mode1: Int, input: Long): Unit
-}
