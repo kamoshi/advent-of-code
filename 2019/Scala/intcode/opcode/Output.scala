f1c749b744048e26f58fd1b863ad77107e67c9e4
kamoshi <18511281+kamoshi@users.noreply.github.com>
2019-12-09 22:25:23 +0100

    Delete Output.scala

--- 2019/Scala/intcode/opcode/Output.scala
+++
@@ -1,7 +0,0 @@
-package intcode.opcode
-
-trait Output extends OpCode
-{
-  /** Executes instruction for given parameters and modes */
-  def output(tape: Array[Long], relative: Long, param1: Long, mode1: Int): Long
-}
