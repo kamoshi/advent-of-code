a802558779c38b61b3bf485f7baaae63de44544c
kamoshi <18511281+kamoshi@users.noreply.github.com>
2019-12-09 22:21:05 +0100

    Update OpCode6.scala

--- 2019/Scala/intcode/opcode/OpCode6.scala
+++ 2019/Scala/intcode/opcode/OpCode6.scala
@@ -1,5 +1,12 @@
 package intcode.opcode
 
+/**
+ * Jump-If-False
+ * If the first parameter is zero, it sets the instruction pointer to the value from the second parameter.
+ * Otherwise, it does nothing.
+ *
+ * checkConditionAndJump -> returns boolean for condition and int for pointer
+ */
 case object OpCode6 extends Jump
 {
   /** length of an instruction */
