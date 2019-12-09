ce2a2d48055c898dc41190b7c979102bbfbbe054
kamoshi <18511281+kamoshi@users.noreply.github.com>
2019-12-09 22:23:43 +0100

    Update OpCode3.scala

--- 2019/Scala/intcode/opcode/OpCode3.scala
+++ 2019/Scala/intcode/opcode/OpCode3.scala
@@ -1,13 +1,15 @@
 package intcode.opcode
 
-/** Input */
-case object OpCode3 extends Input
+/** Input
+ *  Takes a single integer as input and saves it to the position given by its only parameter.
+ */
+case object OpCode3 extends OpCode
 {
   /** length of an instruction */
   override val length: Int = 2
 
   /** Executes instruction for given parameters and modes */
-  override def input(tape: Array[Long], relative: Long, param1: Long, mode1: Int, input: Long): Unit =
+  def input(tape: Array[Long], relative: Long, param1: Long, mode1: Int, input: Long): Unit =
   {
     writer(tape, relative, param1, mode1, input)
   }
