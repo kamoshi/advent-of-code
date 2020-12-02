4722b81c8f73660a781798e9bfc87df2a0f359c2
kamoshi <18511281+kamoshi@users.noreply.github.com>
2020-12-02 08:47:35 +0100

    Update day02.py

--- 2020/Python/day02.py
+++ 2020/Python/day02.py
@@ -2,12 +2,11 @@ def part1():
     result = 0
     with open("input2.txt") as f:
         for line in f:
-            (policy, char, passcode) = line.split()
+            (policy, (char,_), passcode) = line.split()
             min_p, max_p = policy.split("-")
-            number_of_char = passcode.count(char[0])
+            number_of_char = passcode.count(char)
             if int(min_p) <= number_of_char <= int(max_p):
                 result += 1
-        
         print(result)
 
 
@@ -15,9 +14,9 @@ def part2():
     result = 0
     with open("input2.txt") as f:
         for line in f:
-            (policy, char, passcode) = line.split()
+            (policy, (char,_), passcode) = line.split()
             min_p, max_p = policy.split("-")
-            if (passcode[int(min_p)-1] == char[0] and passcode[int(max_p)-1] != char[0]) or (passcode[int(min_p)-1] != char[0] and passcode[int(max_p)-1] == char[0]):
+            if (passcode[int(min_p)-1] == char and passcode[int(max_p)-1] != char) or (passcode[int(min_p)-1] != char and passcode[int(max_p)-1] == char):
                 result += 1
         print(result)
 
