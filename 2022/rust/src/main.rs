bfe96fbdf5abb49a181be081620228eccfb5d5a9
Maciej Jur <maciej@kamoshi.org>
2022-12-15 20:31:14 +0100

    2022 rust

--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -3,5 +3,19 @@ mod solutions;
 
 
 fn main() {
+    // solutions::day01::run();
+    // solutions::day02::run();
+    // solutions::day03::run();
+    // solutions::day04::run();
+    // solutions::day05::run();
+    // solutions::day06::run();
+    // solutions::day07::run();
+    // solutions::day08::run();
+    // solutions::day09::run();
+    // solutions::day10::run();
+    // solutions::day11::run();
+    // solutions::day12::run();
+    // solutions::day13::run();
+    // solutions::day14::run();
     solutions::day15::run();
 }
