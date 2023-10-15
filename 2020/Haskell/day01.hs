ba4dc0e1f62a0e1e34a894ce7c0dcfba21b8113b
Maciej Jur <maciej@kamoshi.org>
2023-10-15 18:30:04 +0200

    haskell: 2020 01

--- 2020/Haskell/day01.hs
+++ 2020/Haskell/day01.hs
@@ -1,7 +1,41 @@
-main = do  
-        contents <- readFile "input.txt"
-        let list = map (read :: String -> Int) (words contents)
-        let p1 = head [((a, b), a*b) | a <- list, b <- list, a+b == 2020]
-        let p2 = head [((a,b,c),a*b*c) | a <- list, b <- list, c <- list, a+b+c == 2020]
-        print(p1)
-        print(p2)
+module Day01 where
+
+import Data.List (tails)
+import Data.Maybe (fromMaybe)
+
+
+example :: [Integer]
+example =
+  [ 1721
+  , 979
+  , 366
+  , 299
+  , 675
+  , 1456
+  ]
+
+scout :: Int -> [[Integer]] -> Maybe [[Integer]]
+scout _ []    = Nothing
+scout 0 _     = Just []
+scout 1 (h:_) = Just $ map (:[]) h
+scout n (h:t) = do
+  a <- map (head h:) <$> scout (n-1) t
+  let b = fromMaybe [] $ scout n t
+  pure $ a <> b
+
+
+solve :: Int -> [Integer] -> Integer
+solve n = product . head . filter ((2020==) . sum) . fromMaybe [] . scout n . tails
+
+solve1 :: [Integer] -> Integer
+solve1 = solve 2
+
+solve2 :: [Integer] -> Integer
+solve2 = solve 3
+
+main :: IO ()
+main = do
+  contents <- readFile "../.inputs/01.txt"
+  let list = map read $ words contents
+  print $ solve1 list
+  print $ solve2 list
