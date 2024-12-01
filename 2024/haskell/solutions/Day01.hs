ff4da68c1467adb91f9efdc1e02be5dea26b4f79
Maciej Jur <maciej@kamoshi.org>
2024-12-01 10:30:25 +0100

    haskell: 2024 01 a

--- 2024/haskell/solutions/Day01.hs
+++ 2024/haskell/solutions/Day01.hs
@@ -1,18 +1,48 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Day01 (day) where
 
-import Advent (Day, mkDay)
+import Advent (Day, mkDay, (|>))
+import Data.Bifunctor (bimap)
+import Data.List qualified as List
 import Data.Text (Text)
+import Data.Text qualified as Text
+import Debug.Trace (traceShowId)
 
-parse :: Text -> Either String Int
-parse _ = Right 33
+sample :: Text
+sample =
+  "3   4\n\
+  \4   3\n\
+  \2   5\n\
+  \1   3\n\
+  \3   9\n\
+  \3   3"
 
--- $> print . parse $ "33"
+parse :: Text -> Either String ([Int], [Int])
+parse text =
+  text
+    |> Text.lines
+    |> List.map (List.map (read @Int . Text.unpack) . Text.splitOn "   ")
+    |> List.transpose
+    |> parsePair
+  where
+    parsePair = \case
+      [as, bs] -> Right (as, bs)
+      _ -> Left "Malformed input"
 
-solveA :: Int -> Int
-solveA = (+ 1)
+-- $> fmap (solveA) (parse sample)
 
-solveB :: Int -> Int
-solveB = (* 2)
+solveA input =
+  input
+    |> bimap List.sort List.sort
+    |> uncurry List.zip
+    |> List.map (abs . uncurry (-))
+    |> sum
+
+solveB = const 2
 
 day :: Day
 day = mkDay 1 parse solveA solveB
