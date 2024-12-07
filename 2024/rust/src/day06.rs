79fb9b9f040f5ca3d7dcaa2645af0eb249485564
Maciej Jur <maciej@kamoshi.org>
2024-12-07 14:24:20 +0100

    rust: 2024 07 a

--- 2024/rust/src/day06.rs
+++ 2024/rust/src/day06.rs
@@ -1,123 +1,52 @@
-use std::collections::HashSet;
-
 use crate::advent::{day, Error};
 
-day!(6, parse, solve_a, solve_b);
+day!(7, parse, solve_a, solve_b);
 
-type Point = (usize, usize);
-type Cache = Box<[Vec<Point>]>;
-type Input = (Point, Point, Cache, Cache);
-
-enum Dir {
-    U,
-    D,
-    L,
-    R,
-}
-
-impl Dir {
-    fn turn_right(&self) -> Self {
-        match self {
-            Dir::U => Dir::R,
-            Dir::D => Dir::L,
-            Dir::L => Dir::U,
-            Dir::R => Dir::D,
-        }
-    }
-}
+type Input = Vec<(i64, Vec<i64>)>;
 
 fn parse(text: &str) -> Result<Input, Error> {
-    let cols = text.lines().next().map(str::len).unwrap();
+    let mut data = vec![];
 
-    let mut guard = (0, 0);
-    let mut desks = vec![];
-    let mut rows = 0;
+    for line in text.lines() {
+        let (outcome, numbers) = line.split_once(':').ok_or("Malformed data")?;
 
-    for (row, line) in text.lines().enumerate() {
-        rows += 1;
+        let outcome = outcome.parse()?;
+        let numbers = numbers
+            .split_whitespace()
+            .map(str::parse)
+            .collect::<Result<_, _>>()?;
 
-        for (col, char) in line.chars().enumerate() {
-            match char {
-                '#' => desks.push((row, col)),
-                '^' => guard = (row, col),
-                _ => (),
-            }
-        }
+        data.push((outcome, numbers))
     }
 
-    let mut hash_rows: Cache = Box::from(vec![vec![]; rows]);
-    let mut hash_cols: Cache = Box::from(vec![vec![]; cols]);
-
-    for point @ (row, col) in desks {
-        hash_rows[row].push(point);
-        hash_cols[col].push(point);
-    }
-
-    Ok((guard, (rows, cols), hash_rows, hash_cols))
+    Ok(data)
 }
 
-fn solve_a((guard, (rows, cols), hash_rows, hash_cols): &Input) -> usize {
-    let mut pos = *guard;
-    let mut dir = Dir::U;
-
-    let mut visited = HashSet::<Point>::from_iter([pos]);
-
-    loop {
-        let desks = match dir {
-            Dir::L | Dir::R => hash_rows[pos.0].as_slice(),
-            Dir::U | Dir::D => hash_cols[pos.1].as_slice(),
-        };
-
-        let first = match dir {
-            Dir::U => desks.iter().rev().find(|p| p.0 < pos.0),
-            Dir::D => desks.iter().find(|p| p.0 > pos.0),
-            Dir::L => desks.iter().rev().find(|p| p.1 < pos.1),
-            Dir::R => desks.iter().find(|p| p.1 > pos.1),
-        };
-
-        if let Some(first) = first {
-            let (step_row, step_col) = match dir {
-                Dir::U => (-1, 0),
-                Dir::D => (1, 0),
-                Dir::L => (0, -1),
-                Dir::R => (0, 1),
-            };
-
-            let mut curr = pos;
-            loop {
-                let next = (
-                    (curr.0 as isize + step_row) as usize,
-                    (curr.1 as isize + step_col) as usize,
-                );
-
-                if next == (first.0, first.1) {
-                    break;
-                }
-
-                curr = next;
-                visited.insert(curr);
-            }
-
-            pos = curr;
-            dir = dir.turn_right();
-        } else {
-            let path = match dir {
-                Dir::U => 0..pos.0,
-                Dir::D => pos.0..*rows,
-                Dir::L => 0..pos.1,
-                Dir::R => pos.1..*cols,
-            };
-
-            match dir {
-                Dir::U | Dir::D => visited.extend(path.map(|row| (row, pos.1))),
-                Dir::L | Dir::R => visited.extend(path.map(|col| (pos.0, col))),
-            };
-
-            break;
-        }
+fn check(res: i64, acc: i64, numbers: &[i64]) -> bool {
+    if numbers.is_empty() {
+        return res == acc;
     }
 
-    visited.len()
+    let a = {
+        let acc = acc + numbers[0];
+        acc <= res && check(res, acc, &numbers[1..])
+    };
+
+    let b = {
+        let acc = acc * numbers[0];
+        acc <= res && check(res, acc, &numbers[1..])
+    };
+
+    a || b
+}
+
+fn solve_a(data: &Input) -> i64 {
+    data.iter()
+        .filter_map(|&(result, ref numbers)| match check(result, 0, numbers) {
+            true => Some(result),
+            false => None,
+        })
+        .sum()
 }
 
 fn solve_b(b: &Input) -> usize {
@@ -130,23 +59,22 @@ mod test {
     use indoc::indoc;
 
     const SAMPLE: &str = indoc! {"
-        ....#.....
-        .........#
-        ..........
-        ..#.......
-        .......#..
-        ..........
-        .#..^.....
-        ........#.
-        #.........
-        ......#...
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
     "};
 
     #[test]
     fn a() {
-        let input = parse(SAMPLE).unwrap();
-        let result = solve_a(&input);
+        let parsed = parse(SAMPLE).unwrap();
+        let result = solve_a(&parsed);
 
-        assert_eq!(result, 41);
+        assert_eq!(result, 3749);
     }
 }
