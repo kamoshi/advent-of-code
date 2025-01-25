ad2523b0850301ce995be95e7a76c922765ff4aa
Maciej Jur <maciej@kamoshi.org>
2025-01-25 16:00:48 +0100

    rust: 2024 09 a

--- 2024/rust/src/day09.rs
+++ 2024/rust/src/day09.rs
@@ -2,22 +2,94 @@ use crate::advent::{day, Error};
 
 day!(9, parse, solve_a, solve_b);
 
-type Input = Vec<u32>;
+type Input = Vec<Node>;
 
-fn parse(text: &str) -> Result<Input, Error> {
-    // let text = "2333133121414131402";
-    text.trim()
-        .chars()
-        .map(|c| {
-            c.to_digit(10)
-                .ok_or_else(|| format!("parse error on {c}").into())
-        })
-        .collect()
+#[derive(Clone, Copy)]
+enum Node {
+    File(u64, u64),
+    Hole(u64),
 }
 
-fn solve_a(data: &Input) -> u32 {
-    // println!("{:?}", data);
-    1
+fn parse(text: &str) -> Result<Input, Error> {
+    let mut buffer = Vec::with_capacity(text.len());
+
+    for (index, char) in text.trim().chars().enumerate() {
+        let parsed = char
+            .to_digit(10)
+            .ok_or_else(|| format!("parse error on {char}"))? as u64;
+
+        buffer.push(match index % 2 == 0 {
+            true => Node::File(parsed, index as u64 / 2),
+            false => Node::Hole(parsed),
+        })
+    }
+
+    Ok(buffer)
+}
+
+fn solve_a(data: &Input) -> u64 {
+    let mut iter = data.iter().copied();
+    let mut checksum = 0;
+    let mut leftover = None;
+    let mut idx = 0;
+
+    while let Some(next) = iter.next() {
+        match next {
+            Node::File(size, id) => {
+                for ptr in idx..idx + size {
+                    checksum += ptr * id;
+                }
+
+                idx += size;
+            }
+            Node::Hole(mut hole) => {
+                while hole > 0 {
+                    let (left, id) = match leftover {
+                        // if there are leftover blocks for old files
+                        Some(blocks) => blocks,
+                        // if not we need to grab new blocks from the back
+                        None => loop {
+                            match iter.next_back() {
+                                Some(Node::File(size, id)) => break (size, id),
+                                Some(Node::Hole(_)) => continue,
+                                None => return checksum,
+                            }
+                        },
+                    };
+
+                    // if there is more blocks than hole
+                    if left > hole {
+                        for ptr in idx..idx + hole {
+                            checksum += ptr * id;
+                        }
+
+                        leftover = Some((left - hole, id));
+                        idx += hole;
+                        hole = 0;
+                        continue;
+                    }
+
+                    // fill hole with whatever we have right now
+                    for ptr in idx..idx + left {
+                        checksum += ptr * id;
+                    }
+
+                    leftover = None;
+                    idx += left;
+                    hole -= left;
+                }
+            }
+        }
+    }
+
+    // flush leftover blocks
+    if let Some((left, id)) = leftover {
+        for ptr in idx..idx + left {
+            checksum += ptr * id;
+        }
+    }
+
+    checksum
 }
 
 fn solve_b(data: &Input) -> u32 {
